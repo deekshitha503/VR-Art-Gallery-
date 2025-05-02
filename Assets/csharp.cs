69f85629370a7993d91958978dbcb76e615a4971
deekshitha503 <deekshithamalepate13@gmail.com>
2025-05-02 17:30:12 +0530

    First Commit

---
+++ Assets/csharp.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class SceneMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float moveZ = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Translate(new Vector3(moveX, 0, moveZ));
+    }
+}
